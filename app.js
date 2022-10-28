b7e29ed00a5d7099c1dfdd23c8a9e2617323ea95
Vengatesh M <40466166+vengateshm@users.noreply.github.com>
2022-10-28 12:56:59 +0530

    added welcome endpoint

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const port = process.env.PORT || 3001;
 
 app.get("/", (req, res) => res.type('html').send(html));
 
+app.get("/welcome", (req, res) => res.status(200).json({ message: 'Welcome!' });
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 
