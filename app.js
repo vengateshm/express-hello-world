047527e8cbcf66d40b1237e6f966bdd0968952a2
Vengatesh M <40466166+vengateshm@users.noreply.github.com>
2022-10-28 13:11:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const app = express();
+
+require('dotenv').config();
+
 const port = process.env.PORT || 3001;
 
 app.get("/", (req, res) => res.type('html').send(html));
 
-app.get("/welcome", (req, res) => res.status(200).json({message: 'Welcome!'}));
+app.get("/welcome", (req, res) => res.status(200).json({message: process.env.WELCOME_MESSAGE}));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
